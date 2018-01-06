11a9c32f52c9b6b33d5c85ebfeccc022f0965f01
enjing <578951992@qq.com>
2018-01-06 14:25:00 +0800

    增加视频模型

---
+++ app/model/video.js
@@ -0,0 +1,48 @@
+'video strict';
+
+module.exports = app => {
+	const mongoose = app.mongoose;
+	const {
+		ObjectId
+	} = mongoose.Schema.Types;
+	const VideoSchema = new mongoose.Schema({
+		title: {
+			type: String,
+			default: '',
+		},
+		description: {
+			type: String,
+			default: '',
+		},
+		thumb: {
+			type: String,
+			default: '',
+		},
+		content: {
+			type: Text,
+			default: '',
+		},
+		categoryId: [ObjectId],
+		views: {
+			type: Number,
+			default: 0,
+		},
+		status: {
+			type: Number,
+			default: 1,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
+		updatedAt: {
+			type: Date,
+			default: Date.now,
+		},
+		isDeleted: {
+			type: Boolean,
+			default: false,
+		},
+	});
+	return mongoose.model('Video', VideoSchema);
+};
